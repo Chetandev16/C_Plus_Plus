271d0bd287aa016e0541a4ee3e1806a013d145c6
chetan pathak <chetanpathak45@gmail.com>
2021-08-26 16:33:01 +0530

    new commit

---
+++ array/char array/largest_word.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   cin.ignore();
+   char a[n+1];
+   cin.getline(a, n);
+   cin.ignore();
+   int maxlen=0, currlen=0,i=0;
+   while(true)
+   {
+     if(a[i]==' ' || a[i]=='\0')
+     {
+         if(currlen>maxlen)
+         maxlen = currlen;
+      currlen=0;  
+     }
+     currlen++;
+     if(a[i]=='\0')
+     {
+         break;
+     }
+     i++;
+   }
+   cout<<maxlen-1;
+  return 0;
+}
