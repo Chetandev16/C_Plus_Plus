11fb054d6d01f5ad024949fdc5762158a286fa50
chetan pathak <chetanpathak45@gmail.com>
2021-10-18 20:51:52 +0530

    new commti

---
+++ backtracking/n-queen.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isSafe(int** arr, int x,int y,int n)
+{
+    for(int i=0;i<x;i++)
+    {
+        if(arr[i][y]==1){
+            return false;
+        }
+    }
+
+    int row=x,col=y;
+
+    while(row>=0 && col>=0)   //left digonal check!
+    {
+        if(arr[row][col] == 1)
+        return false;
+
+        row--;
+        col--;
+    }
+
+    row=x,col=y;
+
+    while(row>=0 && col<n)   //right digonal check!
+    {
+        if(arr[row][col] == 1)
+        return false;
+
+        row--;
+        col++;
+    }
+
+    return true;
+
+}
+
+bool nqueen(int** arr, int x, int n)
+{
+    if(x>=n)
+    {
+        return true;
+    }
+
+  for(int i=0;i<n;i++)
+  {
+    if(isSafe(arr,x,i,n))
+    {
+      arr[x][i] = 1 ;
+      if(nqueen(arr,x+1,n)){
+          return true;
+      }
+
+      arr[x][i] = 0 ;//backtrack
+    }
+   }
+
+   return false;
+  }
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int** arr = new int*[n];
+
+    for(int i=0;i<n;i++)
+    {
+        arr[i] = new int[n];
+        for(int j=0;j<n;j++){
+          arr[i][j] = 0;
+        }
+    }
+
+   if(nqueen(arr,0,n)){
+       for(int i=0;i<n;i++)
+       {
+           for(int j=0;j<n;j++)
+           {
+               cout<<arr[i][j]<<" ";
+           }
+           cout<<endl;
+       }
+   }
+    return 0;
+}
