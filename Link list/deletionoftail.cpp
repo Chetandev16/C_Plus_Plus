0d03da48580ccfbfae633ef7f7919f734627c094
Chetan Pathak <70676486+Chetandev16@users.noreply.github.com>
2021-12-30 20:14:11 +0530

    new commit

---
+++ Link list/deletionoftail.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printLinklist(Node *node)
+{
+    Node *curr = node;
+    while (curr != NULL)
+    {
+        cout << (curr->data) << " ";
+        curr = curr->next;
+    }
+}
+
+Node *deleteNode(Node *node)
+{
+    if (node == NULL)
+        return NULL;
+    if (node->next == NULL)
+    {
+        delete (node->next);
+        return NULL;
+    }
+
+    Node *curr = node;
+    while (curr->next->next != NULL)
+    {
+        curr = curr->next;
+    }
+    delete (curr->next);
+    curr->next = NULL;
+
+    return node;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+
+    head = deleteNode(head);
+    printLinklist(head);
+    return 0;
+}
